495a57ce01c77914932b0577b81b968bb66d944f
BeereMgM <maxl.grimmi1001@gmail.com>
2023-09-17 17:51:15 +0200

    Setup File

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 
-VERSION = '0.0.5'
+VERSION = '0.0.6'
 DESCRIPTION = 'ezWrite to files package!'
 LONG_DESCRIPTION = 'A simple package that lets you write faster to files! Watch the whole documentation at https://github.com/BeereMgM/ezWrite'
 
